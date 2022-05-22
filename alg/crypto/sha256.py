b35977d26e031459297e128ef799c77370a353cf
Randy Sim <randysim9@gmail.com>
2022-05-22 17:07:41 -0400

    Implemented sha256 in python

--- alg/crypto/sha256.py
+++ alg/crypto/sha256.py
@@ -1,9 +1,13 @@
 # sha-256 Implementation in Python
-# Author: Randy Sim, Completion-Date:
+# Author: Randy Sim, Completion-Date: 05/22/2022
 # referenced:
 # https://www.researchgate.net/file.PostFileLoader.html?id=534b393ad3df3e04508b45ad&assetKey=AS%3A273514844622849%401442222429260
 # https://csrc.nist.gov/csrc/media/publications/fips/180/4/final/documents/fips180-4-draft-aug2014.pdf
 # https://www.youtube.com/watch?v=f9EbD6iY9zI
+
+# Struggles:
+# the "+" in the guide is not the same thing as XOR
+# the plus is addition mod 2^32
 
 """
 steps:
@@ -48,39 +52,136 @@ def RotR(A, n):  # rotate
     # input: binary string of word, output: rotate it to the right n times
     l = len(A)
     rot_len = n % l
-    return A[l - rot_len:] + A[rot_len-1:l - rot_len]
+    return A[l - rot_len:] + A[:l - rot_len]
 
 
 def ShR(A, n):  # shift
     l = len(A)
-    return "0" * n + A[:l - n]
+    return "0" * min(n, l) + A[:l - n]
 
 
 def Ch(X, Y, Z):  # choose (Determine output from Y and Z based on X)
-    return (X & Y) ^ ((~X) & Z)
+    return '{0:b}'.format((int(X, 2) & int(Y, 2)) ^ ((~int(X, 2)) & int(Z, 2))).rjust(len(X), "0")
 
 
 def Maj(X, Y, Z):  # chooses based on the majority of the bits X, Y, and Z
-    return (X & Y) ^ (X & Z) ^ (Y & Z)
+    return '{0:b}'.format((int(X, 2) & int(Y, 2)) ^ (int(X, 2) & int(Z, 2)) ^ (int(Y, 2) & int(Z, 2))).rjust(len(X), "0")
+
+
+def o0(x):
+    return '{0:b}'.format(int(RotR(x, 7), 2) ^ int(RotR(x, 18), 2) ^ int(ShR(x, 3), 2)).rjust(len(x), "0")
+
+
+def o1(x):
+    return '{0:b}'.format(int(RotR(x, 17), 2) ^ int(RotR(x, 19), 2) ^ int(ShR(x, 10), 2)).rjust(len(x), "0")
+
+
+def e0(x):
+    return '{0:b}'.format(int(RotR(x, 2), 2) ^ int(RotR(x, 13), 2) ^ int(RotR(x, 22), 2)).rjust(len(x), "0")
+
+
+def e1(x):
+    return '{0:b}'.format(int(RotR(x, 6), 2) ^ int(RotR(x, 11), 2) ^ int(RotR(x, 25), 2)).rjust(len(x), "0")
 
 
 def preprocess(bstr):
     # accepts binary string input
     # returns list of 512 bit blocks
 
+    blocks = []
+    l = len(bstr)
     bstr += "1"  # separater
 
-    blocks = []
-    n = int(len(bstr) / 512)
+    n = int((l + 1) / 512)
 
-    for i in range(n):
+    r = (l + 1) - (n * 512)  # remaining
+    zeros = 0
+    if r > 448:
+        zeros = 960 - r
+    else:
+        zeros = 448 - r
+
+    bstr += "0" * zeros
+
+    # add 64 bit number rep
+    bstr += '{0:064b}'.format(l)
+
+    for i in range(n + 1):
         # 8 bits per char
-        blocks.append(bstr[i*64:(i + 1) * 64])
+        blocks.append(bstr[(i*512):(i + 1) * 512])
 
     return blocks
 
-# generate hash values, fractional values of square roots of the first 8 prime numbers
+
+def mod32a(st):
+    val = 0
+
+    for x in st:
+        val += int(x, 2)
+
+    return val % (2 ** 32)
 
 
-val = input("enter string to be encrypted: ")
-bin = to_bin(val)
+def g_next(schedule, t):
+    return '{0:b}'.format(mod32a([o1(schedule[t - 2]), schedule[t - 7], o0(schedule[t-15]), schedule[t-16]])).rjust(len(schedule[0]), "0")
+
+# create message schedule for a block
+
+
+def c_sche(m_block):
+    schedule = []
+    for i in range(16):  # create the first 16 using existing block
+        schedule.append(m_block[i * 32:(i + 1) * 32])
+    # generate the rest of the schedule
+    for k in range(48):
+        schedule.append(g_next(schedule, k+16))
+
+    return schedule
+
+
+def compress(blocks):
+    a = "{0:b}".format(I[0]).rjust(32, "0")
+    b = "{0:b}".format(I[1]).rjust(32, "0")
+    c = "{0:b}".format(I[2]).rjust(32, "0")
+    d = "{0:b}".format(I[3]).rjust(32, "0")
+    e = "{0:b}".format(I[4]).rjust(32, "0")
+    f = "{0:b}".format(I[5]).rjust(32, "0")
+    g = "{0:b}".format(I[6]).rjust(32, "0")
+    h = "{0:b}".format(I[7]).rjust(32, "0")
+
+    for block in blocks:
+        schedule = c_sche(block)
+
+        for i in range(len(schedule)):
+            t1 = "{0:b}".format(mod32a([e1(e), Ch(e, f, g),
+                                        h, "{0:b}".format(K[i]).rjust(32, "0"), schedule[i]])).rjust(32, "0")
+            t2 = "{0:b}".format(mod32a([e0(a), Maj(a, b, c)])).rjust(32, "0")
+
+            h = g
+            g = f
+            f = e
+            e = "{0:b}".format(mod32a([d, t1])).rjust(32, "0")
+            d = c
+            c = b
+            b = a
+            a = "{0:b}".format(mod32a([t1, t2])).rjust(32, "0")
+    a = "{0:b}".format(mod32a(["{0:b}".format(I[0]), a])).rjust(32, "0")
+    b = "{0:b}".format(mod32a(["{0:b}".format(I[1]), b])).rjust(32, "0")
+    c = "{0:b}".format(mod32a(["{0:b}".format(I[3]), c])).rjust(32, "0")
+    e = "{0:b}".format(mod32a(["{0:b}".format(I[4]), d])).rjust(32, "0")
+    f = "{0:b}".format(mod32a(["{0:b}".format(I[5]), e])).rjust(32, "0")
+    g = "{0:b}".format(mod32a(["{0:b}".format(I[6]), f])).rjust(32, "0")
+    h = "{0:b}".format(mod32a(["{0:b}".format(I[7]), g])).rjust(32, "0")
+
+    return '{0:06x}'.format(int(a, 2)) + '{0:06x}'.format(int(b, 2)) + '{0:06x}'.format(int(c, 2)) + '{0:06x}'.format(int(d, 2)) + '{0:06x}'.format(int(e, 2)) + '{0:06x}'.format(int(f, 2)) + '{0:06x}'.format(int(g, 2)) + '{0:06x}'.format(int(h, 2))
+
+
+def sha256(str):
+    str_bin = to_bin(str)
+    prep = preprocess(str_bin)
+    return compress(prep)
+
+
+val = input("Enter string to be hashed:")
+ans = sha256(val)
+print(ans)
